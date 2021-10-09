0ffa077b5602c6e1bb1771e1b5c8135c492d95cc
Smolyar Tonya <am.smolyar94@gmail.com>
2021-10-09 19:22:13 +0300

    add security methods

--- src/main/java/ru/netology/demo/config/SecurityConfig.java
+++ src/main/java/ru/netology/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ru.netology.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -16,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("Evgeniy").password(encoder.encode("password1")).roles("ADMIN")
                 .and()
-                .withUser("Pavel").password(encoder.encode("password2")).roles("USER")
+                .withUser("Pavel").password(encoder.encode("password2")).roles("STAFF")
                 .and()
                 .withUser("Olga").password(encoder.encode("password3")).roles("USER")
                 .and()
--- src/main/java/ru/netology/demo/controller/Controller.java
+++ src/main/java/ru/netology/demo/controller/Controller.java
@@ -1,11 +1,15 @@
 package ru.netology.demo.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.demo.dao.Person;
 import ru.netology.demo.service.Service;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -26,10 +30,46 @@ public class Controller {
         return service.getPersonsByAge(age);
     }
 
-    @GetMapping("/persons/by-name-surname")
+    @GetMapping("/persons/by-name-surname/for-user")
+    @Secured("ROLE_USER")
     public Person getPersonByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
         return service.getPersonByNameAndSurname(name, surname);
     }
+
+    @GetMapping("/persons/by-name-surname/for-staff")
+    @RolesAllowed("ROLE_STAFF")
+    public Person getPersonByNameAndSurname1(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
+        return service.getPersonByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/persons/by-name-surname/for-staff-admin")
+    @PreAuthorize("hasRole('ROLE_STAFF') or hasRole('ROLE_ADMIN')")
+    public Person getPersonByNameAndSurname2(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
+        return service.getPersonByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/persons/by-name-surname/for-staff-admin2")
+    @PostAuthorize("hasRole('ROLE_STAFF') or hasRole('ROLE_ADMIN')")
+    public Person getPersonByNameAndSurname3(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
+        return service.getPersonByNameAndSurname(name, surname);
+    }
+
+    @GetMapping("/persons/by-name-surname/for-username")
+    @PreAuthorize("#username==authentication.principal.username")
+    public Person getPersonByNameAndSurname4(@RequestParam("username") String username, @RequestParam("name") String name, @RequestParam("surname") String surname) {
+        System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
+        return service.getPersonByNameAndSurname(name, surname);
+    }
+
+
+//    @GetMapping("/persons/by-name-surname")
+//    public Person getPersonByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+//        System.out.println("User " + SecurityContextHolder.getContext().getAuthentication().getName() + " works with table");
+//        return service.getPersonByNameAndSurname(name, surname);
+//    }
 
 }
